8d178aebbf463a68b57723e1f152e0a57360d5bf
vickyleonardo23 <vickyleonardo23@gmail.com>
2024-12-04 09:56:58 +0000

    update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,12 +14,12 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-indigo-200 dark:bg-gray-900">
+    <body class="font-sans text-gray-900 antialiased" style="background-image: url('https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=2340&auto=format&fit=crop') !important; background-size: cover !important; background-position: center !important;">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0  dark:bg-gray-900">
             <div>
-                <a href="/">
+                {{-- <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+                </a> --}}
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
